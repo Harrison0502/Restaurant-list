326c5e4069a3669ffd6d23985c810d487bed57cd
harrison0502 <harrison0502@gmail.com>
2023-05-10 01:22:24 +0800

    feat: "add search、show function"

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-const express=require('express')
-const app=express()
+const express = require('express')
+const app = express()
+const port = 3000
+const restaurantList = require('./restaurant.json')
 
-const port=3000
+// require express-handlebars here
+const exphbs = require('express-handlebars')
+const { restart } = require('nodemon')
 
-app.get('/',(req,res)=>{
-  res.send(`This is my resturant list`)
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  const restaurants = restaurantList.results
+  res.render(`index`, { restaurants })
 })
 
-app.listen(port,()=>{
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id === Number(req.params.restaurant_id))
+  res.render('show', { restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase()))
+  res.render(`index`, { restaurants, keyword: req.query.keyword })
+})
+
+app.listen(port, () => {
   console.log('Express is running now')
 })
