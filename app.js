88e6d5b12ed8bd218ebe31824dd9378d0e7eeee9
harrison0502 <harrison0502@gmail.com>
2023-06-11 14:38:24 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const port = 3000
 const RestaurantList = require('./models/restaurant')//載入restaurant model
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
-
-
-
+// 引用 body-parser
+const bodyParser = require('body-parser')
 // require express-handlebars here
 const { restart } = require('nodemon')
 
@@ -31,26 +30,39 @@ db.once('open',()=>{
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars')
-
 // setting static files
 app.use(express.static('public'))
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 
+//瀏覽餐廳
 app.get('/', (req, res) => {
   RestaurantList.find()
     .lean()
-    .then(restaurant => res.render('index', { restaurants })) // 將資料傳給 index 樣板
+    .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
     .catch(error => console.log(error))
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = RestaurantList.results.find(restaurant => restaurant.id === Number(req.params.restaurant_id))
-  res.render('show', { restaurant })
+//新增頁面
+app.get('/restaurants/new',(req,res)=>{
+  return res.render('new')
 })
 
-app.get('/search', (req, res) => {
-  const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase()))
-  res.render(`index`, { restaurants, keyword: req.query.keyword })
+//新增餐廳
+app.post('/restaurants',(req,res)=>{
+   RestaurantList.create(req.body)
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
+// app.get('/restaurants/:restaurant_id', (req, res) => {
+//   const restaurant = RestaurantList.results.find(restaurant => restaurant.id === Number(req.params.restaurant_id))
+//   res.render('show', { restaurant })
+// })
+
+// app.get('/search', (req, res) => {
+//   const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase()))
+//   res.render(`index`, { restaurants, keyword: req.query.keyword })
+// })
 
 app.listen(port, () => {
   console.log('Express is running now')
