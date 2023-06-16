21221399498146de4001c3de19b4437028700ae1
harrison0502 <harrison0502@gmail.com>
2023-06-17 01:34:09 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -15,9 +15,28 @@ router.get('/', (req, res) => {
 //搜尋餐廳
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim().toLowerCase()
+  const sortBy=req.query.sortBy
+  let sortName=''
+  let sortObject={}
+  if (sortBy === 'az') {
+    sortObject.name = 'asc'
+    sortName="A->Z"
+  } else if (sortBy === 'za') {
+    sortObject.name = 'desc'
+    sortName = "Z->A"
+  } else if (sortBy === 'category') {
+    sortName = "類別"
+  } else if (sortBy === 'location') {
+    sortObject.location = 'asc'
+    sortName = "地區"
+  } else if (sortBy === 'rating') {
+    sortObject.rating = -1//降冪，由高到低
+    sortName = "評分"
+  }
   return RestaurantList.find({ name: { $regex: new RegExp(keyword, "i") } })//找尋name 包含keyword的餐廳(不分大小寫)
     .lean()
-    .then(restaurants => res.render('index', { restaurants, keyword })) // 將資料傳給 index 樣板
+    .sort(sortObject)
+    .then(restaurants => res.render('index', { restaurants , keyword , sortName})) // 將資料傳給 index 樣板
     .catch(error => console.log(error))
 
 })
