32419cd79db6300d641e9fed4f9ecf26bda2cf0e
harrison0502 <harrison0502@gmail.com>
2023-07-11 18:57:19 +0800

    feat:Add users modules & Login.hbs & register.hbs

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 // 引入 home 模組程式碼
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
-
+const users = require('./modules/users')
 // request 導向模組 
+
+router.use('/users',users)
 router.use('/', home)
 router.use('/restaurants',restaurants)
 
