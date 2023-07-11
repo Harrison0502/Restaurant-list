3073f03c5a1bbfdd42420557b51397fa3f1a633a
harrison0502 <harrison0502@gmail.com>
2023-07-12 01:35:50 +0800

    feat: add bcrypt.js

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
+const bcrypt = require('bcryptjs')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
+
 
 module.exports = app => {
 
@@ -14,10 +16,12 @@ module.exports = app => {
         if (!user) {
           return done(null, false, req.flash('warning_msg', '此 Email 尚未註冊。'))
         }
-        if (user.password !== password) {
-          return done(null, false, req.flash('warning_msg', '密碼錯誤。'))
-        }
-        return done(null, user)
+        return bcrypt.compare(password, user.password).then(isMatch => {
+          if (!isMatch) {
+            return done(null, false, { message: 'Email or Password incorrect.' })
+          }
+          return done(null, user)
+        })
       })
       .catch(err => done(err, false));
   }))
