b5661e51d475d991914c08a29679d9e138b2a63a
harrison0502 <harrison0502@gmail.com>
2023-07-11 23:50:40 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -28,8 +28,12 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
-
 usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 //將 request 導入路由器
 app.use(routes)
 
